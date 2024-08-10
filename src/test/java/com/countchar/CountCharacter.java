1d4e1a836492e7031822550ef774c55b03c88656
Ravichandran <Priglo@DESKTOP-EQ66RDU.mshome.net>
2024-08-10 15:14:55 +0530

    New Project

---
+++ src/test/java/com/countchar/CountCharacter.java
@@ -0,0 +1,29 @@
+package com.countchar;
+
+public class CountCharacter {
+	
+	
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			String Str = "Udhayan is a good boy";
+			System.out.println(Str);
+			
+			int count = 0;
+			
+			for(int i=0;i<Str.length();i++) {
+				
+				if (Str.charAt(i) != ' ') {
+					
+					count = count+1;
+		
+				}
+				
+			}
+			
+			System.out.println("The total number of characters in the given string is "+count);
+			
+
+		}
+
+}
